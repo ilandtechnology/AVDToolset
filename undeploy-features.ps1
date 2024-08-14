7c322b73fecae4b2460769727dc82f46514ea2bf
Jonildo Santos <jonildo_santos@iland.com.br>
2024-08-14 01:26:33 -0300

    MINOR FIX

--- undeploy-features.ps1
+++ undeploy-features.ps1
@@ -10,18 +10,22 @@ if( -not $? ) { $enableResult = 1 }
 Write-Host "AVD AIB Customization - Uninstall Windows Features : Uninstalling the Printing XPS Services..."
 Disable-WindowsOptionalFeature -Online -FeatureName Printing-XPSServices-Features -NoRestart
 if( -not $? ) { $enableResult = 1 }
-Write-Host "AVD AIB Customization - Uninstall Windows Features : Uninstalling the PowerShell V2..."
-Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root -NoRestart
-if( -not $? ) { $enableResult = 1 }
+# Write-Host "AVD AIB Customization - Uninstall Windows Features : Uninstalling the PowerShell V2..."
+# Disable-WindowsOptionalFeature -Online -FeatureName MicrosoftWindowsPowerShellV2Root -NoRestart
+# if( -not $? ) { $enableResult = 1 }
 Write-Host "AVD AIB Customization - Uninstall Windows Features : Uninstalling the WorkFolders-Client..."
 Disable-WindowsOptionalFeature -Online -FeatureName WorkFolders-Client -NoRestart
 if( -not $? ) { $enableResult = 1 }
 Write-Host "AVD AIB Customization - Uninstall Windows Features : Uninstalling the Windows Media Player..."
 if ([Environment]::OSVersion.Version -ge (New-Object 'Version' 10.0.22000)) { Disable-WindowsOptionalFeature -Online -FeatureName WindowsMediaPlayer -NoRestart } else { Disable-WindowsOptionalFeature -Online -FeatureName MediaPlayback -NoRestart }
 if( -not $? ) { $enableResult = 1 }
+# Remove-WindowsCapability -Online -Name Microsoft.Windows.WordPad~~~~
+# if( -not $? ) { $enableResult = 1 }
+# Remove-WindowsCapability -Online -Name Print.Fax.Scan~~~~0.0.1.0
+# if( -not $? ) { $enableResult = 1 }
 
 # Check the exit code of the installation and cleanup
-if ( $enableResult -eq 0 ) {
+if ($enableResult -eq 0) {
     $stopwatch.Stop()
     $elapsedTime = $stopwatch.Elapsed
     Write-Host "AVD AIB Customization - Uninstall Windows Features : Uninstall successfully."
