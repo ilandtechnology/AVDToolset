21ac537e44b35ab57b6d34d8329ddd725cfa98e7
Jonildo Santos <jonildo_santos@iland.com.br>
2024-08-22 23:43:52 -0300

    MINOR FIX

--- disable-windowsupdate.ps1
+++ disable-windowsupdate.ps1
@@ -6,7 +6,9 @@ If(-not(Test-Path -Path $WindowsUpdatePath)) {
     New-Item -Path $AutoUpdatePath
 }
 
+Set-ItemProperty -Path $WindowsUpdatePath -Name SetDisableUXWUAccess -Value 1
 Set-ItemProperty -Path $AutoUpdatePath -Name NoAutoUpdate -Value 1
+Set-ItemProperty -Path $AutoUpdatePath -Name AUOptions -Value 1
+Set-ItemProperty -Path $AutoUpdatePath -Name EnableFeaturedSoftware -Value 1
 
-
-New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Run" -Name BackInfo -Type String -Value "C:\Windows\Backinfo.exe"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Run" -Name BackInfo -Type String -Value "C:\Windows\BackInfo.exe"
