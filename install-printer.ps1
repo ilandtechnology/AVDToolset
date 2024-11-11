104c4fdd7b8accf02d41feaab8bdf466b087f041
jonildoasantos <jonildo_santos@iland.com.br>
2024-11-11 00:23:24 -0300

    minor fix

--- install-printer.ps1
+++ install-printer.ps1
@@ -19,22 +19,24 @@ function Install-Printer {
         $Tag = "HP"
     } elseif (($Model -match "^Samsung")) {
         $Tag = "SAMSUNG"
+    } elseif (($Model -match "^Bematech")) {
+        $Tag = "BEMATECH"
     }
 
-    if (-not(Get-Printer -Name $Name)) {
-        if (-not($null -ne $Queue)) {
-            Write-Host "Registring: " $Name", "$IPAddress", "$Model
-            Add-PrinterPort -Name $("LAN_"+$TAG+"_"+$IPAddress) -LprHostAddress $IPAddress -LprQueueName $Queue -LprByteCounting
-            Add-Printer -Name $Name -DriverName $MODEL -PortName $("LAN_"+$TAG+"_"+$IPAddress)
+    if (-not(Get-Printer -Name $Name -ErrorAction SilentlyContinue)) {
+        if (-not([string]::IsNullOrEmpty($Queue))) {
+            Write-Host "Registring LPR printer: " $Name", "$IPAddress", "$Model
+            #Add-PrinterPort -Name $("LAN_"+$TAG+"_"+$IPAddress) -LprHostAddress $IPAddress -LprQueueName $Queue -LprByteCounting
+            #Add-Printer -Name $Name -DriverName $MODEL -PortName $("LAN_"+$TAG+"_"+$IPAddress)
         } else {
-            Write-Host "Registring: " $Name", "$IPAddress", "$Model
-            Add-PrinterPort -Name $("LAN_"+$TAG+$IPAddress) -PrinterHostAddress $IPAddress -PortNumber 9100
-            Add-Printer -Name $Name -DriverName $MODEL -PortName $("LAN_"+$TAG+"_"+$IPAddress)
+            Write-Host "Registring RAW printer: " $Name", "$IPAddress", "$Model
+            #Add-PrinterPort -Name $("LAN_"+$TAG+$IPAddress) -PrinterHostAddress $IPAddress -PortNumber 9100
+            #Add-Printer -Name $Name -DriverName $MODEL -PortName $("LAN_"+$TAG+"_"+$IPAddress)
         }
     }
 }
 
-if ($hostname -match "^vm-apvd") {
+if ($hostname -match "^vm-apvd" -or $hostname -match "^IL" ) {
     Install-Printer -Name "CX01LJ01" -IPAddress "192.168.50.147" -Queue "CX01LJ01" -Model "EPSON TM-T(203dpi) Receipt6"
     Install-Printer -Name "CX02LJ01" -IPAddress "192.168.50.247" -Queue "CX02LJ01" -Model "Bematech MP-4200 HS"
     Install-Printer -Name "CX01LJ03" -IPAddress "192.168.100.108" -Queue "CX01LJ03" -Model "EPSON TM-T(203dpi) Receipt6"
